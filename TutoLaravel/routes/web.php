3bf49d79f0b8dd24e69af5ec64a99b697b1f1994
dzeko003 <berenismassamba@gmail.com>
2024-06-24 10:38:40 +0200

    v1

--- TutoLaravel/routes/web.php
+++ TutoLaravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/blog' , function (){
-    return "Bonjour !";
+Route::prefix('/blog')->group(function () {
+
+    Route::get('/', function (Request $request) {
+        return [
+            // "name" => $request->path(),
+            // "name" => $request->url(),
+            // "name" => $request->all(),
+            // "name" => $request->input('name', 'Berenis'),
+            // "article" => "Article 1"
+            "link" => \route('blog.show',['slug' => "article" , "id" => 13])
+        ];
+    })->name('blog.index');
+    
+    
+    Route::get('/{slug}-{id}', function (string $slug , string $id , Request $request) {
+        return [
+            "slug" => $slug,
+            "id" => $id,
+            "name" => $request->input('name')
+        ];
+    })->where(
+        [
+            "id" => '[0-9]+',
+            "slug" => '[a-z0-9\-]+'
+        ]
+    )->name('blog.show');
 });
+
