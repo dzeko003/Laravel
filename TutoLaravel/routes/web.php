6a76cbe3ec4ff9108d7eed5c727efdbf3eb4a813
dzeko003 <berenismassamba@gmail.com>
2024-07-03 15:04:31 +0200

    debut des controlleurs

--- TutoLaravel/routes/web.php
+++ TutoLaravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,28 +22,46 @@ Route::get('/', function () {
 Route::prefix('/blog')->group(function () {
 
     Route::get('/', function (Request $request) {
-        return [
-            // "name" => $request->path(),
-            // "name" => $request->url(),
-            // "name" => $request->all(),
-            // "name" => $request->input('name', 'Berenis'),
-            // "article" => "Article 1"
-            "link" => \route('blog.show',['slug' => "article" , "id" => 13])
-        ];
-    })->name('blog.index');
+        // $post = new Post();
+        // $post->title = "Mon second article";
+        // $post->slug = 'mon-second-article';
+        // $post->content = "Ceci est mon second article";
+        // // $post->save();
+
+        $posts = Post::paginate(25);
+        
+        return $posts;
+
+        // return [
+        //     // "name" => $request->path(),
+        //     // "name" => $request->url(),
+        //     // "name" => $request->all(),
+        //     // "name" => $request->input('name', 'Berenis'),
+        //     // "article" => "Article 1"
+        //     "link" => \route('blog.show', ['slug' => "article", "id" => 13])
+        // ];
+    })->name('index');
     
     
-    Route::get('/{slug}-{id}', function (string $slug , string $id , Request $request) {
-        return [
-            "slug" => $slug,
-            "id" => $id,
-            "name" => $request->input('name')
-        ];
+    Route::get('/{slug}-{id}', function (string $slug, string $id, Request $request) {
+       
+        $post = Post::findOrFail($id);
+
+        if ($post->slug !== $slug) {
+            return redirect()->route('show', ['slug' => $post->slug, 'id' => $post->id]);
+        }
+
+        return $post;
+       
+        // return [
+        //     "slug" => $slug,
+        //     "id" => $id,
+        //     "name" => $request->input('name')
+        // ];
     })->where(
         [
             "id" => '[0-9]+',
             "slug" => '[a-z0-9\-]+'
         ]
-    )->name('blog.show');
+    )->name('show');
 });
-
