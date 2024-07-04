a18637a7859dee2ca44a12cb88d13456148208ef
dzeko003 <berenismassamba@gmail.com>
2024-07-04 10:34:50 +0200

    controleur et Blade

--- TutoLaravel/app/Http/Controllers/BlogController.php
+++ TutoLaravel/app/Http/Controllers/BlogController.php
@@ -4,23 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    public function index():Paginator{
-        
-        return $posts = Post::paginate(25);
+    public function index():View{
+        $posts = Post::paginate(1);
+
+        return view('blog.index',[
+            'posts' => $posts
+        ]);
     }
 
-    public function show(string $slug , string $id) : RedirectResponse | Post{
+    public function show(string $slug , string $id) : RedirectResponse | View{
         $post = Post::findOrFail($id);
 
         if ($post->slug !== $slug) {
-            return to_route('blog.show',['slug' => $post->slug, 'id' => $post->id]);
+            return redirect()->route('blog.show', ['slug' => $post->slug, 'id' => $post->id]);
         }
 
-        return $post;
+       
+        return view('blog.show',[
+            'post' => $post
+        ]);
     }
 }
--- TutoLaravel/routes/web.php
+++ TutoLaravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,46 +20,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('/blog')->group(function () {
+Route::prefix('/blog')->name('blog.')->controller(BlogController::class)->group(function () {
 
-    Route::get('/', function (Request $request) {
-        // $post = new Post();
-        // $post->title = "Mon second article";
-        // $post->slug = 'mon-second-article';
-        // $post->content = "Ceci est mon second article";
-        // // $post->save();
-
-        $posts = Post::paginate(25);
-        
-        return $posts;
-
-        // return [
-        //     // "name" => $request->path(),
-        //     // "name" => $request->url(),
-        //     // "name" => $request->all(),
-        //     // "name" => $request->input('name', 'Berenis'),
-        //     // "article" => "Article 1"
-        //     "link" => \route('blog.show', ['slug' => "article", "id" => 13])
-        // ];
-    })->name('index');
+    Route::get('/','index')->name('index');
     
     
-    Route::get('/{slug}-{id}', function (string $slug, string $id, Request $request) {
-       
-        $post = Post::findOrFail($id);
-
-        if ($post->slug !== $slug) {
-            return redirect()->route('show', ['slug' => $post->slug, 'id' => $post->id]);
-        }
-
-        return $post;
-       
-        // return [
-        //     "slug" => $slug,
-        //     "id" => $id,
-        //     "name" => $request->input('name')
-        // ];
-    })->where(
+    Route::get('/{slug}-{id}','show')->where(
         [
             "id" => '[0-9]+',
             "slug" => '[a-z0-9\-]+'
