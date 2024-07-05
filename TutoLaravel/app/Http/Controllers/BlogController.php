6d4cdb1e852d125a392333596252ffba32db28ef
dzeko003 <berenismassamba@gmail.com>
2024-07-05 11:25:42 +0200

    ide helper

--- TutoLaravel/app/Http/Controllers/BlogController.php
+++ TutoLaravel/app/Http/Controllers/BlogController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BlogFilterRequest;
 use App\Models\Post;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BlogController extends Controller
 {
-    public function index():View{
+    public function index(BlogFilterRequest $request):View{
+       
         $posts = Post::paginate(1);
 
         return view('blog.index',[
@@ -18,8 +21,8 @@ class BlogController extends Controller
         ]);
     }
 
-    public function show(string $slug , string $id) : RedirectResponse | View{
-        $post = Post::findOrFail($id);
+    public function show(string $slug , Post $post) : RedirectResponse | View{
+        $post = Post::findOrFail($post);
 
         if ($post->slug !== $slug) {
             return redirect()->route('blog.show', ['slug' => $post->slug, 'id' => $post->id]);
--- TutoLaravel/routes/web.php
+++ TutoLaravel/routes/web.php
@@ -23,12 +23,15 @@ Route::get('/', function () {
 Route::prefix('/blog')->name('blog.')->controller(BlogController::class)->group(function () {
 
     Route::get('/','index')->name('index');
-    
-    
-    Route::get('/{slug}-{id}','show')->where(
+
+    Route::get('/{slug}-{post}','show')->where(
         [
-            "id" => '[0-9]+',
+            "post" => '[0-9]+',
             "slug" => '[a-z0-9\-]+'
         ]
     )->name('show');
+
+    // Route::get('/{post:slug}','show')->where([
+    //     'post' => '[a-z0-9\-]+'
+    // ])->name('show');
 });
